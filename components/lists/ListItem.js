7e044fa7a5ae6d318a958e8ae73c184f3efb7074
bixingjian <bixingjian19@163.com>
2023-10-14 23:18:43 -0400

    update Invite screen, add picture, add lists components, Text, and Screen components

---
+++ components/lists/ListItem.js
@@ -0,0 +1,76 @@
+/**
+ * Description: This is to show the content of the Card Component
+ */
+
+import React from "react";
+import { View, StyleSheet, Image, TouchableHighlight } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import Text from "../Text";
+import colors from "../../config/colors";
+
+function ListItem({
+  title,
+  subTitle,
+  image,
+  IconComponent,
+  onPress,
+  renderRightActions,
+}) {
+  return (
+    <GestureHandlerRootView>
+      <Swipeable renderRightActions={renderRightActions}>
+        <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
+          <View style={styles.container}>
+            {IconComponent}
+            {image && <Image style={styles.image} source={image} />}
+            <View style={styles.detailsContainer}>
+              <Text style={styles.title} numberOfLines={1}>
+                {title}
+              </Text>
+              {subTitle && (
+                <Text style={styles.subTitle} numberOfLines={2}>
+                  {subTitle}
+                </Text>
+              )}
+            </View>
+            <MaterialCommunityIcons
+              name="chevron-right"
+              size={25}
+              color={colors.medium}
+            />
+          </View>
+        </TouchableHighlight>
+      </Swipeable>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    flexDirection: "row",
+    padding: 15,
+    backgroundColor: colors.white, // the background of Accountscreen is light, so we can have list as white to show up.
+  },
+  detailsContainer: {
+    flex: 1,
+    paddingLeft: 10,
+    justifyContent: "center",
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+  },
+  subTitle: {
+    color: colors.medium,
+  },
+  title: {
+    fontWeight: "500",
+  },
+});
+
+export default ListItem;
