0653076101c17be0d4c21b27883be150e1103e5e
bixingjian <79216992+bixingjian@users.noreply.github.com>
2023-11-29 14:37:46 -0500

    add useeffect to calendar, change the startDate to be the selected, add empty date

--- screens/CalendarViewScreen.js
+++ screens/CalendarViewScreen.js
@@ -1,78 +1,84 @@
-import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet } from "react-native";
 import { Agenda } from "react-native-calendars";
 import { FIRESTORE_DB } from "../firebase/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, onSnapshot, getDoc, doc } from "firebase/firestore";
 
-export default function CalendarView() {
+export default function CalendarView({ route, navigation }) {
   const [items, setItems] = useState({});
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
 
   const timeToString = (time) => {
     const date = new Date(time);
     return date.toISOString().split("T")[0];
   };
 
-  const loadItems = async (day) => {
-    const newItems = { ...items }; // Use a new object to avoid mutating the state directly
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(FIRESTORE_DB, "trips", route.params.tripId, "events"),
+      async (snapshot) => {
+        let newItems = { ...items };
+        let dates = [];
 
-    const eventsSnap = await getDocs(
-      collection(FIRESTORE_DB, "trips", "Berlin", "events")
-    );
-    const events = [];
-    eventsSnap.forEach((doc) => {
-      events.push({
-        eventID: doc.detailedName,
-        ...doc.data(),
-      });
-    });
-    console.log(events);
-
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = timeToString(time);
-
-        if (!newItems[strTime]) {
-          newItems[strTime] = [];
-        }
-      }
-
-      events.forEach((event) => {
-        const dateKey = event.date;
-
-        newItems[dateKey] = [];
-      });
-
-      events.forEach((event) => {
-        const dateKey = event.date;
-
-        newItems[dateKey].push({
-          detailedName: event.detailedName,
-          date: event.date,
+        // get dates:
+        const DateRef = await getDoc(doc(FIRESTORE_DB, "trips", "Berlin"));
+        sDate = DateRef.data().startDate;
+        setStartDate(sDate);
+        eDate = DateRef.data().endDate;
+        setEndDate(eDate);
+        console.log(startDate);
+        console.log(endDate);
+        // Collecting all dates from the snapshot
+        snapshot.forEach((doc) => {
+          const event = { eventID: doc.detailedName, ...doc.data() };
+          dates.push(new Date(event.date));
         });
-      });
 
-      setItems(newItems);
-    }, 100);
-  };
+        if (dates.length > 0) {
+          // Finding the smallest date or a random date from the dataset
+          // For smallest date: const baseDate = new Date(Math.min(...dates));
+          // For random date:
+          const baseDate = dates[0];
+          console.log(timeToString(baseDate));
+          // Pre-populate around the base date
+          for (let i = -30; i < 30; i++) {
+            const time = baseDate.getTime() + i * 24 * 60 * 60 * 1000;
+            const strTime = timeToString(time);
+            newItems[strTime] = [];
+          }
+
+          // Adding events to their respective dates
+          snapshot.forEach((doc) => {
+            const event = { eventID: doc.detailedName, ...doc.data() };
+            const dateKey = event.date;
+
+            newItems[dateKey].push({
+              detailedName: event.detailedName,
+              date: event.date,
+            });
+          });
+        }
+
+        setItems(newItems);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    return () => unsubscribe();
+  }, []);
 
   const renderItem = (item, firstItemInDay) => {
     return (
-      <View
-        style={{
-          marginRight: 20,
-          marginTop: 30,
-          padding: 20,
-          backgroundColor: "white",
-          borderRadius: 5,
-        }}
-      >
+      <View style={styles.item}>
         <Text>{item.detailedName}</Text>
       </View>
     );
   };
 
-  renderEmptyDate = () => {
+  const renderEmptyDate = () => {
     return (
       <View style={styles.emptyDate}>
         <Text></Text>
@@ -84,8 +90,7 @@ export default function CalendarView() {
     <View style={{ flex: 1 }}>
       <Agenda
         items={items}
-        loadItemsForMonth={loadItems}
-        selected={"2023-11-26"}
+        selected={startDate}
         renderItem={renderItem}
         renderEmptyDate={renderEmptyDate}
       />
@@ -100,7 +105,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     marginRight: 10,
-    marginTop: 17,
+    marginTop: 30,
   },
   emptyDate: {
     height: 15,
